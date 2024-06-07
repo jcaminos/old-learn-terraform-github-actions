52952e6e5b5b4f20b86df45c6eefad5d7c3dadf2
Javier Caminos <javier.caminos@avature.net>
2024-06-07 20:28:05 +0000

    change por again by 3rd time

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8080
+    from_port   = 8079
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
